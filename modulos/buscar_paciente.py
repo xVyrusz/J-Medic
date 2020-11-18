e31453dc905cf47bde7178ba11c5d44dbb95aa6a
Carreon1301 <c4rreon1234@gmail.com>
2020-11-18 01:56:08 -0600

    hola

--- modulos/buscar_paciente.py
+++ modulos/buscar_paciente.py
@@ -17,6 +17,39 @@ class WindowTwo(QtWidgets.QMainWindow):
         self.boton_BuscarNombre.clicked.connect(self.switch)
         self.actionRegresar.setShortcut("Ctrl+R")
         self.actionRegresar.triggered.connect(self.switch)
+        self.validar()
+
+    def validar(self):
+        self.input_BuscarId.textChanged.connect(self.validar_id)
+        self.input_BuscarNombre.textChanged.connect(self.validar_nombre)
+        pass
+
+    def validar_id(self):
+        aidi = self.input_BuscarId.text()
+        validar = re.match("^\d{1,}$", telefono, re.I)
+        if aidi == "":
+            self.input_BuscarId.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.input_BuscarId.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.input_BuscarId.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_nombre(self):
+        nombre = self.input_BuscarNombre.text()
+        validar = re.match(
+            "^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}$", nombre, re.I)
+        if nombre == "":
+            self.input_BuscarNombre.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.input_BuscarNombre.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.input_BuscarNombre.setStyleSheet("border: 2px solid green;")
+            return True
 
     def switch(self):
         self.switch_window.emit()

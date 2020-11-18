d9635359de80b0db3c4cbe1535fe70b8368829f9
Carreon1301 <c4rreon1234@gmail.com>
2020-11-17 22:02:30 -0600

    cambio

--- modulos/registrar_medicos.py
+++ modulos/registrar_medicos.py
@@ -16,6 +16,91 @@ class WindowTwo(QtWidgets.QMainWindow):
         self.Boton_Guardar.clicked.connect(self.switch)
         self.actionRegresar.setShortcut("Ctrl+R")
         self.actionRegresar.triggered.connect(self.switch)
+        self.validar()
+
+    def validar(self):
+        self.input_Nombre.textChanged.connect(self.validar_nombre)
+        self.input_ApellidoP.textChanged.connect(self.validar_apellidoP)
+        self.input_ApellidoM.textChanged.connect(self.validar_apellidoM)
+        self.input_Cedula.textChanged.connect(self.validar_cedula)
+        self.input_Telefono.textChanged.connect(self.validar_telefono)
+        self.input_turno.textChanged.connect(self.seleccionar_turno)
+        pass
+
+
+    def validar_nombre(self):
+        nombre = self.input_Nombre.text()
+        validar = re.match(
+            "^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}$", nombre, re.I)
+        if nombre == "":
+            self.input_Nombre.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.input_Nombre.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.input_Nombre.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_apellidoP(self):
+        apellido = self.input_ApellidoP.text()
+        validar = re.match(
+            "^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}$", apellido, re.I)
+        if apellido == "":
+            self.input_ApellidoP.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.input_ApellidoP.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.input_ApellidoP.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_apellidoM(self):
+        apellido = self.input_ApellidoM.text()
+        validar = re.match(
+            "^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}$", apellido, re.I)
+        if apellido == "":
+            self.input_ApellidoM.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.input_ApellidoM.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.input_ApellidoM.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_cedula(self):
+        cedula = self.input_Cedula.text()
+        validar = re.match("^\d{1,}$", cedula, re.I)
+        if cedula == "":
+            self.input_Cedula.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.input_Cedula.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.input_Cedula.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_telefono(self):
+        telefono = self.input_Telefono.text()
+        validar = re.match("^\d{10}$", telefono, re.I)
+        if telefono == "":
+            self.input_Telefono.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.input_Telefono.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.input_Telefono.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def seleccionar_turno(self):
+        value = self.input_Turno_2.value()
+        self.input_turno.setText(str(value))
+
+    
 
     def switch(self):
         self.switch_window.emit()

bf8b8a0f2a0b26da08a00cf7928bd7c7a5c91a51
Eduardo Cisneros <cisnnerosbcrra@hotmail.com>
2020-11-19 18:21:24 -0600

    arreglando

--- calandohash.py
+++ calandohash.py
@@ -1,12 +1,12 @@
 import bcrypt
 import os
 
-password = "jalo"
+password = "contraseña"
 
 hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt(10))
-
+hashed = b'$2b$10$p6f6qaN3GTNUCYR0TTtJdeb8pcLcQ4cRo/rxFRNd44y67qvGDcqje'
 print(hashed)
-print(len(hashed))
+print(hashed.decode())
 if bcrypt.checkpw(password.encode(), hashed):
     print("si jala")
 else:
